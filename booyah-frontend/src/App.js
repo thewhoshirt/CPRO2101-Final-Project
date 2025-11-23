3a77cdd6e2f887487bb03d7eb20b3bb57c9b8a9e
thewhoshirt <s.embury.36@gmail.com>
2025-11-23 15:25:36 -0700

    About page fixed to work with styled - components

--- booyah-frontend/src/App.js
+++ booyah-frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/NavBar';
 import {
   BrowserRouter as Router,
   Routes,
--- booyah-frontend/src/pages/AboutElements.js
+++ booyah-frontend/src/pages/AboutElements.js
@@ -1,52 +1,67 @@
 import styled from "styled-components";
 
-export const body = styled.body`
-
-    text-align:center;
+export const BodyContainer = styled.div`
+    text-align: center;
     background-color: #FCFAF2;
     position: relative;
+`;
 
-`
-// .booyah-logo{
-//     width: 20%;
-// }
+export const BooyahLogo = styled.img`
+    width: 20%;
+`;
 
-// .heading{
-//     display: flex;
-//     justify-content: space-evenly;
-//     align-items: center;
-//     margin-bottom: 5%;
-//     margin-left: 3%;
-//     width: 90%;
-// }
+export const Heading = styled.div`
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    margin-bottom: 5%;
+    margin-left: 3%;
+    width: 90%;
+`;
 
+export const HeadingText = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
 
-// .pink-heading{
-//     color: #FF5FBE;
-//     padding: 10px;
-// }
+export const PinkHeading = styled.h2`
+    color: #FF5FBE;
+    padding: 10px;
+`;
 
-// .intro-text{
-//     background-color: #C9F2DE;
-//     padding: 2%;
-//     border:solid black 2px;
-// }
+export const IntroText = styled.div`
+    background-color: #C9F2DE;
+    padding: 2%;
+    border: solid black 2px;
+`;
 
-// .image{
-//     width:50%;
-//     height:100%;
-//     border: solid black 2px;
-// }
+export const Image = styled.img`
+    width: 50%;
+    height: 100%;
+    border: solid black 2px;
+`;
 
-// .pillars-div, .purpose-div{
-//     height:fit-content;
-//     display: flex;
-//     justify-content: center;
-// }
+export const PillarsDiv = styled.div`
+    height: fit-content;
+    display: flex;
+    justify-content: center;
+`;
 
-// .about-pillars, .about-purpose{
-//     padding:5%;
-//     border: solid black 2px;
-// }
+export const PurposeDiv = styled.div`
+    height: fit-content;
+    display: flex;
+    justify-content: center;
+`;
+
+export const AboutPillars = styled.div`
+    padding: 5%;
+    border: solid black 2px;
+`;
+
+export const AboutPurpose = styled.div`
+    padding: 5%;
+    border: solid black 2px;
+`;
 
 
