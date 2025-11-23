96d0e306927ee0ab58b7f8944ed82fb61ff9cff6
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-11-23 14:09:38 -0700

    seeding and seed data for database

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
     ProductName: {type: String, required: true, unique: true},
-    ProductPrice: {type: Float, required: true}
+    ProductPrice: {type: Number, required: true}
 });
 module.exports = mongoose.model('Product',productSchema);
