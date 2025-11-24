2eee52187db86849f34f01b20c2f2e6828ab9d94
janelle-and <janelle.andrews@rdpolytech.ca>
2025-11-23 17:33:47 -0700

    Home Page started

--- booyah-frontend/src/pages/index.js
+++ booyah-frontend/src/pages/index.js
@@ -1,17 +1,20 @@
 import React from "react";
+import { BodyContainer, BooyahLogo, Image, ImageDiv, Graphic1, Graphic2 } from "../styles/HomeElements";
 
 const Home = () => {
    return( 
-    <div  
-    // style={{
-    //     display: 'flex',
-    //     justifyContent: 'centre',
-    //     alignItems: 'centre',
-    //     height: '100vh'
-    //   }}
-      >
-        <h1>Booyah Bagels... Home?</h1>
-    </div>
+    <BodyContainer>
+        <Graphic1 src="Booyah Assets\Graphics\Squiggle14_Swell.png" alt="squiggle"/>
+        <BooyahLogo src="/Booyah Assets/GIFS/Booyah Bagels Logo_hero.gif" alt="Booyah Logo"/>
+        <Graphic2 src="Booyah Assets\Graphics\Squiggle11_Sunshine.png" alt="squiggle"/>
+        <ImageDiv>
+            <Image src="Booyah Assets\Brand images\Bagels on wall.jpg" alt="Bagels on the wall"/>
+            <Image src="Booyah Assets\Brand images\bag with box.jpg" alt="Bagels on the wall"/>
+            <Image src="Booyah Assets\Brand images\Hand roll 1.jpg" alt="Bagels on the wall"/>
+        </ImageDiv>
+        <h1>RIDICULOUSLY DELICIOUS</h1>
+    </BodyContainer>
+
    );
 
 };
