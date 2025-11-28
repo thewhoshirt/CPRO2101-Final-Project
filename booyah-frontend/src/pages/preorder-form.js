c6fe836c9add1c58eff1e1badd33beb0f7d9e848
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-11-27 19:53:29 -0700

    preorder form styling and validations for basic info

--- booyah-frontend/src/pages/preorder-form.js
+++ booyah-frontend/src/pages/preorder-form.js
@@ -1,93 +1,138 @@
 import React, { useState } from 'react';
+import {
+    BodyContainer,
+    FormBody,
+    Input,
+    TextArea,
+    BooyahLogo,
+    List,
+    ListItems
+} from "../styles/PreorderElements";
 function PreorderForm(){
     const [Name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [phone, setPhone] = useState("");
     const [date, setDate] = useState("");
     const [notes, setNotes] = useState("");
+    const [errors, setErrors] = useState({ name: "", email: "", phone: "", date: "", notes: "" });
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(
-            Name,
-            email,
-            phone,
-            date,
-            notes
-        );
-    };
-    const handleReset = () => {
+        let formErrors = { name: "", email: "", phone: "", date: "", notes: "" };
+        let isValid = true;
+
+        if (Name.trim() === "") {
+            formErrors.name = "Name is required.";
+            isValid = false;
+        }
+        if (email.trim() === "") {
+            formErrors.email = "Email is required.";
+            isValid = false;
+        }
+        if (phone.trim() === "") {
+            formErrors.phone = "Phone is required.";
+            isValid = false;
+        }
+        if(date.trim() === ""){
+            formErrors.date = "Date is required";
+            isValid = false;
+        }
+        if (notes.trim() === "") {
+            formErrors.notes = "Notes are required.";
+            isValid = false;
+        }
+
+        if (!isValid) {
+            setErrors(formErrors);
+            return;
+        }
+
+        console.log(Name, email, phone,date, notes);
+        
         setName("");
         setEmail("");
         setPhone("");
-        setDate("");
         setNotes("");
+        setDate("");
+        setErrors({
+            name: "",
+            email: "",
+            phone: "",
+            date: "",
+            notes: ""
+        });
     };
     return (
-        <div>
-            <img src="..\..\public\Booyah Assets\Logo Horizontal\Bi-Color\Web\BooyahBagels-Logo-Horizontal-Windbreaker and pretty in pink.png" alt="Booyah Logo"/>
-            <h2>Thanks for pre-ordering our bagels! A few things to note:</h2>
-            <ul>
-                <li> We require a minimum 3 days' notice for all pre-orders. Pre-orders are for pick up only (no delivery option available).</li>
-                <li>Pre-orders can be picked up during store hours after 8:00 AM on weekdays and after 10:00 AM on weekends. We are closed Tuesdays.</li>
-                <li>Please note we do have a maximum number of bagels we can allocate to pre-orders per day. If required, we will contact you to go over details and availability before the order is complete.</li>
-                <li>If you need to cancel an order, it must be done prior to 2:00 PM the day before pick up. All orders cancelled after the cut off will not be refunded</li>
-            </ul>
-            <form>
-                <label for="name">Name* </label>
-                <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    value={Name}
-                    onChange={(e) => setName(e.target.value) }
-                    placeholder="Name"
-                    required
-                />
-                <label for="email">Email* </label>
-                <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value) }
-                    placeholder="Email"
-                    required
-                />
-                <label for="phone">Phone*</label>
-                <input
-                    type="tel"
-                    name="phone"
-                    id="phone"
-                    value={phone}
-                    onChange={(e) => setPhone(e.target.value) }
-                    placeholder="Phone"
-                    required
-                />
-                <label for="date">Date</label>
-                <input
-                    type="date"
-                    name="date"
-                    id="date"
-                    value={date}
-                    onChange={(e) => setDate(e.target.value)}
-                    placeholder='Date'
-                    required
-                />
-                <label for="notes">Notes</label>
-                <textarea
-                    type="text"
-                    name="notes"
-                    id="notes"
-                    value={notes}
-                    onChange={(e) => setNotes(e.target.value)}
-                    placeholder='Notes'
-                    required
-                />
-                <button type="reset" value="reset" onClick={() => handleReset()} > Reset </button>
-                <button type="submit" value="Submit" onClick={(e) => handleSubmit(e)} > Submit </button>
-            </form>
-        </div>
-        
+        <BodyContainer>
+             <FormBody>
+                 <BooyahLogo src="Booyah Assets\Logo Horizontal\Bi-Color\Web\BooyahBagels-Logo-Horizontal-Windbreaker and pretty in pink.png" alt="Booyah Logo"/>
+                 <h2>Thanks for pre-ordering our bagels! A few things to note:</h2>
+                 <List>
+                     <ListItems>We require a minimum 3 days' notice for all pre-orders. Pre-orders are for pick up only (no delivery option available).</ListItems>
+                     <ListItems>Pre-orders can be picked up during store hours after 8:00 AM on weekdays and after 10:00 AM on weekends. We are closed Tuesdays.</ListItems>
+                     <ListItems>Please note we do have a maximum number of bagels we can allocate to pre-orders per day. If required, we will contact you to go over details and availability before the order is complete.</ListItems>
+                     <ListItems>If you need to cancel an order, it must be done prior to 2:00 PM the day before pick up. All orders cancelled after the cut off will not be refunded</ListItems>
+                 </List>
+                 <h1>Pre order</h1>
+                 <form>
+                      <Input
+                          type="text"
+                          name="name"
+                          id="name"
+                          value={Name}
+                          onChange={(e) => setName(e.target.value) }
+                          placeholder="Name"
+                          required
+                      />
+                      {errors.name && <div style={{ color: 'red' }}>{errors.name}</div>}
+                      <br/>
+                      <Input
+                          type="email"
+                          name="email"
+                          id="email"
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value) }
+                          placeholder="Email"
+                          required
+                      />
+                      {errors.email && <div style={{ color: 'red' }}>{errors.email}</div>}
+                      <br/>
+                      <Input
+                          type="tel"
+                          name="phone"
+                          id="phone"
+                          value={phone}
+                          onChange={(e) => setPhone(e.target.value) }
+                          placeholder="Phone"
+                          required
+                      />
+                      {errors.phone && <div style={{ color: 'red' }}>{errors.phone}</div>}
+                      <br/>
+                      <TextArea
+                          type="text"
+                          name="notes"
+                          id="notes"
+                          value={notes}
+                          onChange={(e) => setNotes(e.target.value)}
+                          placeholder='Notes'
+                          required
+                      />
+                      {errors.notes && <div style={{ color: 'red' }}>{errors.notes}</div>}
+                      <br/>
+                      <Input
+                        type="date"
+                        name="date"
+                        id="date"
+                        value={date}
+                        onChange={(e) => setDate(e.target.value)}
+                        placeholder='Date'
+                        required
+                      />
+                      {errors.date && <div style={{ color: 'red' }}>{errors.date}</div>}
+                      <br/>
+                      <button type="submit" value="Submit" onClick={(e) => handleSubmit(e)} > Submit </button>
+                    </form>
+                </FormBody>
+             </BodyContainer>
   );
 }
 export default PreorderForm;
---
+++ booyah-frontend/src/styles/PreorderElements.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+export const BodyContainer = styled.div`
+    text-align: center;
+    background-color: #FCFAF2;
+    position: relative;
+`;
+export const FormBody = styled.div`
+    text-align: center;
+    width: 90%;
+    margin: 0 auto;
+    background-color: #eff5f2;
+`
+export const Input = styled.input`
+    border: 1px solid black;
+    border-radius: 4px;
+    width: 80%;
+    height: 50px;
+    margin-bottom: 20px;
+    &:focus{
+        outline: 2px solid #B275BB;
+    }
+`
+export const TextArea = styled.textarea`
+    border: 1px solid black;
+    border-radius: 4px;
+    width: 80%;
+    height: 90px;
+    margin-bottom: 20px;
+    &:focus{
+        outline: 2px solid #B275BB;
+    }
+`
+export const BooyahLogo = styled.img`
+    width: 35%;
+`
+export const List = styled.ul`
+    text-align: left;
+`
+export const ListItems = styled.li`
+    margin-bottom: 5px;
+    margin-left: 10%;
+`
