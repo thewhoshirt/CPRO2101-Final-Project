5511814103505570c0b8af6a35d0d32e2fe18660
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-11-30 13:56:48 -0700

    seed.js comments

--- backend/seeder/seed.js
+++ backend/seeder/seed.js
@@ -1,9 +1,20 @@
 const dotenv = require('dotenv');
-dotenv.config();
+dotenv.config(); // use the .env file
+/**
+ * Import the databaase connection setting and the product modle and route
+ */
 const connectDB = require('../config/db');
 const Product = require('../models/Product');
 const products = require('./products');
+/**
+ * Function to clear and populate the database everytime the backend is run to avoid
+ * having continuity errors between runs of the program
+ */
 const seedDatabase = async () => {
+  /**
+   * Try to connect to the database and then clear and insert the product data then log how mnay items were added
+   * otherwise log the error along with the specified message
+   */
   try {
     await connectDB();
     await Product.deleteMany();
@@ -13,6 +24,9 @@ const seedDatabase = async () => {
     console.error('Seeding failed:', err);
   } 
 };
+/**
+ * Run the seedDatabase() function when the node server is started
+ */
 if (require.main === module) {
   seedDatabase();
 }
