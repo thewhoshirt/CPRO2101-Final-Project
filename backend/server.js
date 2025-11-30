5a9fd6550f94891a13245a1a03bbe763a1fd0130
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-11-30 14:03:16 -0700

    server.js comments

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,29 @@
+/**
+ * Import express, dotenv, and the custom functions/routes required for the backend to work
+ */
 const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const { seedDatabase } = require('./seeder/seed');
-dotenv.config();
-const app = express();
-app.use(express.json());
+const app = express(); // tell the program to use express
+const PORT = process.env.PORT || 5000; // use the port specified in our env file otherwise use port 5000
+const productRoutes = require('./routes/productRoutes');
+dotenv.config(); // tell the program to use the env file
+app.use(express.json()); // allow the program to send data as json
+/**
+ * Allow cross-site request origins so that the google maps API works
+ */
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 	next();
 });
-const PORT = process.env.PORT || 5000;
-const productRoutes = require('./routes/productRoutes');
-app.use('/api',productRoutes);
+app.use('/api',productRoutes); // put the gathered database info at the /api/products endpoint
+/**
+ * Function that runs when the backend program is started, this function tries to connect to the database
+ * then it seeds the database and tells the user what port the program is listening on.
+ * Otherwise the program will log the error and exit
+ */
 const start = async () => {
 	try {
 		await connectDB();
@@ -23,4 +34,4 @@ const start = async () => {
 		process.exit(1);
 	}
 };
-start();
+start(); // run the start() function when the backend is started
