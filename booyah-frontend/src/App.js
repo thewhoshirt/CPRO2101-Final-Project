51ffa27aefaadd0785088d3b48919c79f3198739
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-11-30 14:18:27 -0700

    app.js comments

--- booyah-frontend/src/App.js
+++ booyah-frontend/src/App.js
@@ -1,22 +1,33 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
+import React from 'react'; // import React
+import './App.css'; // import the styles from App.css
+/**
+ * Import the routing functions from React
+ */
 import {
   BrowserRouter as Router,
   Routes,
   Route,
 }from "react-router-dom";
+/**
+ * Import the custome pages and navbar/footer components
+ */
 import Home from './pages';
 import Contact from './pages/contact';
 import About from './pages/about.js';
 import PreorderForm from './pages/preorder-form.js'
 import Menu from './pages/menu.js';
 import Footer from './components/Footer';
-
+import Navbar from './components/Navbar';
 function App() {
   return (
+    /**
+     * Wrap the program in the router so that all of the pages are available
+     */
     <Router>
-      <Navbar />
+      <Navbar /> {/* put the navbar at the top of the page */}
+      {/**
+       * Configure the routes to all of the pages
+       */}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
@@ -24,10 +35,8 @@ function App() {
         <Route path='/contact' element={<Contact />} />
         <Route path='/pre-order' element={<PreorderForm/>}/>
       </Routes>
-      <Footer />
+      <Footer /> {/* put the footer at the bottom of the page */}
     </Router>
-
   );
 }
-
 export default App;
