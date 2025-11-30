45c741222331ee31c0d3d1f785381d4c9b6ca9f8
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-11-30 13:41:22 -0700

    db.js comments

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
+/**
+ * Function that connects to the mongodb local database
+ */
 const connectDB = async () => {
+    /**
+     * Try to connect to the database using the parameters in the env file and log the success
+     * or catch the error and print that to the console before exiting
+     */
     try{
         await mongoose.connect(process.env.MONGO_URI, {});
         console.log("MongoDB connected");
