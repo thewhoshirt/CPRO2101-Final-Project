52866c6e5573f32b001c8a031afb5d1b7ec7144d
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-11-30 13:43:41 -0700

    productController.js comments

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,15 @@
-const Product = require('../models/Product');
-
+const Product = require('../models/Product'); // get the prodcut model
+/**
+ * Function to get all the products from the database
+ * @param {*} req 
+ * @param {*} res 
+ */
 exports.getProducts = async (req, res) => {
+  /**
+   * Try to get the products from the database using the Find() function with no parameters
+   * to get every product, then return that as json to the browser
+   * If there is an error, return an error to the browser with the specified message
+   */
   try {
     const products = await Product.find();
     res.json(products);
