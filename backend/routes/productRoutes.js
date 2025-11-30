6b13f2d22683215cfe660b596d5dda5b139fd569
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-11-30 13:46:32 -0700

    productRoutes.js comments

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router(); // use the express router for easier management
 const productController = require('../controllers/productController');
-
-router.get('/products',productController.getProducts);
-
+router.get('/products',productController.getProducts); // call the getProducts() function to get all the products and put the result on the /products page
 module.exports = router;
