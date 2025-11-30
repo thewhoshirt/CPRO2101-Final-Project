7720971332af20c8c91a87edae7e5dd3fd6a3871
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-11-30 14:07:54 -0700

    footer component comments

--- booyah-frontend/src/components/Footer/index.js
+++ booyah-frontend/src/components/Footer/index.js
@@ -1,16 +1,21 @@
-import { FooterContainer, Logo, SocialLogos } from '../../styles/FooterElements';
-
+import { FooterContainer, Logo, SocialLogos } from '../../styles/FooterElements'; // import the styled components from the footerelements file
 const Footer = () => {
     return (
         <>
             <FooterContainer>
-                <Logo src="Booyah Assets\Monogram\Bi-Color\Web\BooyahBagels-Monogram-Windbreaker and Pretty in pink.png" alt="Booyah Bagels Logo" />
+                <Logo src="Booyah Assets\Monogram\Bi-Color\Web\BooyahBagels-Monogram-Windbreaker and Pretty in pink.png" alt="Booyah Bagels Logo" /> {/*Add the business logo to the footer*/}
+                {/**
+                 * Add the logos for social media presences as well as make the logos links to the respective pages
+                 */}
                 <div>
                     <h1>Spread the good stuff.</h1>
                     <a href="https://www.facebook.com/people/Booyah-Bagels/100091819339135/"><SocialLogos src='/Facebook.avif' alt="facebook Logo"/></a>
                     <a href="https://www.instagram.com/booyahbagels/"><SocialLogos src='/Instagram.avif' alt="instagram Logo"/></a>
                     <a href="https://www.tiktok.com/@booyahbagels"><SocialLogos src='/TikTok.avif' alt="tiktok Logo"/></a>
                 </div>
+                {/**
+                 * Add the address and contact info to the footer
+                 */}
                 <div>
                     <p>#180, 5441 45 ST, RED DEER, AB</p>
                     <p>HELLO@BOOYAHBAGELS.CA</p>
@@ -19,5 +24,4 @@ const Footer = () => {
         </>
     );
 };
-
 export default Footer;
