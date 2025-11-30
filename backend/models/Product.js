33fd4f82a424c5c0f03a0cb4a22e873847da0938
Nico-Hammer <nico.hammer@rdpolytech.ca>
2025-11-30 13:45:26 -0700

    product.js comments

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+/**
+ * Create the mongodb schema for the product which includes product name and price as their respective
+ * datatypes (string and number) and make them both required to add a product to the database
+ */
 const productSchema = new mongoose.Schema({
     ProductName: {type: String, required: true, unique: true},
     ProductPrice: {type: Number, required: true}
